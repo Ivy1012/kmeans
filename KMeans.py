4409e5cab83f7c89d983ccd469943f4577926229
Ivy1012 <35629351+Ivy1012@users.noreply.github.com>
2018-03-25 12:36:43 +0800

    Update KMeans.py

--- KMeans.py
+++ KMeans.py
@@ -11,25 +11,26 @@ import time
 import matplotlib.pyplot as plt  
   
   
-# calculate Euclidean distance  
+# calculate Euclidean distance
+# 计算两个向量之间的欧式距离
 def euclDistance(vector1, vector2):  
-	return sqrt(sum(power(vector2 - vector1, 2)))  #求这两个矩阵的距离，vector1、2均为矩阵
+	return sqrt(sum(power(vector2 - vector1, 2)))  #power(x1, x2)，表示对每一个x1中的每个元素求x2次方，求这两个矩阵的距离，vector1、2均为矩阵
   
 # init centroids with random samples  
 #在样本集中随机选取k个样本点作为初始质心
 def initCentroids(dataSet, k):  
-	numSamples, dim = dataSet.shape   #矩阵的行数、列数 
+	numSamples, dim = dataSet.shape   #矩阵的行数、列数 行数代表有多少个点，列数代表维度
 	centroids = zeros((k, dim))  		#感觉要不要你都可以
 	for i in range(k):  
-		index = int(random.uniform(0, numSamples))  #随机产生一个浮点数，然后将其转化为int型
-		centroids[i, :] = dataSet[index, :]  
-	return centroids  
+		index = int(random.uniform(0, numSamples))  #随机产生一个浮点数，然后将其转化为int型，即随机产生一个中心
+		centroids[i, :] = dataSet[index, :]  #每一行为k个中心的坐标
+	return centroids                            #返回值为一个中心数据集，共k行，每一行代表一个中心
   
 # k-means cluster 
 #dataSet为一个矩阵
 #k为将dataSet矩阵中的样本分成k个类 
 def kmeans(dataSet, k):  
-	numSamples = dataSet.shape[0]  #读取矩阵dataSet的第一维度的长度,即获得有多少个样本数据
+	numSamples = dataSet.shape[0]  #shape[0]代表第一维度的长度，读取矩阵dataSet的第一维度的长度,即获得有多少个样本数据
     # first column stores which cluster this sample belongs to,  
     # second column stores the error between this sample and its centroid  
 	clusterAssment = mat(zeros((numSamples, 2)))  #得到一个N*2的零矩阵
@@ -41,14 +42,14 @@ def kmeans(dataSet, k):
 	while clusterChanged:  
 		clusterChanged = False  
         ## for each sample  
-		for i in range(numSamples):  #range
-			minDist  = 100000.0  
+		for i in range(numSamples):  
+			minDist  = 100000.0    #可以设置为minDist = float('inf') 表示正无穷？
 			minIndex = 0  
             ## for each centroid  
             ## step 2: find the centroid who is closest  
 			#计算每个样本点与质点之间的距离，将其归内到距离最小的那一簇
 			for j in range(k):  
-				distance = euclDistance(centroids[j, :], dataSet[i, :])  
+				distance = euclDistance(centroids[j, :], dataSet[i, :])  #计算的是第j个质心与第i个样点之间的距离
 				if distance < minDist:  
 					minDist  = distance  
 					minIndex = j  
@@ -60,11 +61,12 @@ def kmeans(dataSet, k):
 				clusterChanged = True  
 				clusterAssment[i, :] = minIndex, minDist**2  #两个**表示的是minDist的平方
   
-        ## step 4: update centroids  
+            ## step 4: update centroids  
 		for j in range(k):  
-			#clusterAssment[:,0].A==j是找出矩阵clusterAssment中第一列元素中等于j的行的下标，返回的是一个以array的列表，第一个array为等于j的下标
+			#clusterAssment[:,0].A==j是找出矩阵clusterAssment中第一列元素中等于j的行的下标，返回的是一个以array的列表，
+			#第一个array为等于j的下标
 			pointsInCluster = dataSet[nonzero(clusterAssment[:, 0].A == j)[0]] #将dataSet矩阵中相对应的样本提取出来 
-			centroids[j, :] = mean(pointsInCluster, axis = 0)  #计算标注为j的所有样本的平均值
+			centroids[j, :] = mean(pointsInCluster, axis = 0)  #axis = 0,代表对每一行求平均，得到新的质心，计算标注为j的所有样本的平均值
   
 	print ('Congratulations, cluster complete!')  
 	return centroids, clusterAssment  
@@ -80,7 +82,7 @@ def showCluster(dataSet, k, centroids, clusterAssment):
   
 	mark = ['or', 'ob', 'og', 'ok', '^r', '+r', 'sr', 'dr', '<r', 'pr']  
 	if k > len(mark):  
-		print ("Sorry! Your k is too large! please contact wojiushimogui")  
+		print ("Sorry! please enter a smaller k")  
 		return 1 
      
   
